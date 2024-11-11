4e1544787d33b3b45618fc694dbf609d4724c111
sonud4m <endenn209@gmail.com>
2024-11-11 09:13:25 +0900

    create inputbox

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+use std::io;
+fn main() {
+    println!("Guess the number!");
+
+    println!("Please input your guess.");
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");     //行の読み込みに失敗
+
+    println!("You guessed: {}",guess);  
+}
+
